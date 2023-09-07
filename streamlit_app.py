386feb86609a87238daec614309c5c6f7dc4ac12
RasmusKornbek <35880535+RasmusKornbek@users.noreply.github.com>
2023-09-07 13:01:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,33 @@
 import streamlit as st
+import pandas as pd
+import snowflake.connector
 
-# Initialize connection.
-conn = st.experimental_connection('snowpark')
+# Streamlit app title
+st.title("Snowflake Table Viewer")
 
-# Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+# Get Snowflake secrets from Streamlit Secrets
+snowflake_account = st.secrets["snowflake_account"]
+snowflake_username = st.secrets["snowflake_username"]
+snowflake_password = st.secrets["snowflake_password"]
+snowflake_database = st.secrets["snowflake_database"]
+snowflake_schema = st.secrets["snowflake_schema"]
+snowflake_table = st.secrets["snowflake_table"]
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.NAME} has a :{row.PET}:")
+# Connect to Snowflake
+conn = snowflake.connector.connect(
+    account=snowflake_account,
+    user=snowflake_username,
+    password=snowflake_password,
+    database=snowflake_database,
+    schema=snowflake_schema
+)
 
+# Query Snowflake table
+query = f"SELECT * FROM {snowflake_table}"
+df = pd.read_sql(query, conn)
 
+# Display the table in Streamlit
+st.write(df)
+
+# Close Snowflake connection
+conn.close()
